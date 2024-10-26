0c02f1b60ccd71d72e36367e1c87941ec26b48a5
QAFed <143259597+QAFed@users.noreply.github.com>
2024-10-26 16:29:08 +0300

    cov 9%

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,6 +1,9 @@
 import pytest
 
 from praktikum.bun import Bun
+class TestData:
+    name = 'slim'
+    price = 11.1
 
 class TestBun:
     @pytest.mark.parametrize('test_param', [
@@ -8,7 +11,13 @@ class TestBun:
         'price'
     ])
     def test_class_init_data_create_if_set_correctly(self, test_param):
-        name = 'slim'
-        price =  11.1
-        bun = Bun(name, price)
-        assert getattr(bun, test_param) == locals().get(test_param)
+        bun = Bun(TestData.name, TestData.price)
+        assert getattr(bun, test_param) == getattr(TestData, test_param)
+
+    @pytest.mark.parametrize('test_param', [
+        'name',
+        'price'
+    ])
+    def test_class_getters_return_value(self, test_param):
+        bun = Bun(TestData.name, TestData.price)
+        assert getattr(bun, f'get_{test_param}')() == getattr(TestData, test_param)
