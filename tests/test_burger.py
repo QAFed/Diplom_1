c7322fc8163680bb5dd8c0f050dc2b858ecb78f2
QAFed <143259597+QAFed@users.noreply.github.com>
2024-10-26 17:39:02 +0300

    set buns

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,9 +1,13 @@
 import pytest
 
 from praktikum.burger import Burger
+from praktikum.bun import Bun
 class TestInitData:
     bun = None
     ingredients = []
+
+class TestData:
+    exempl_bun = Bun("exo", 11.22)
 
 class TestBurger:
     @pytest.mark.parametrize('test_param', [
@@ -13,3 +17,10 @@ class TestBurger:
     def test_class_init_data_create_if_set_correctly(self, test_param):
         burger = Burger()
         assert getattr(burger, test_param) == getattr(TestInitData, test_param)
+
+    def test_setter_buns(self):
+        burger = Burger()
+        burger.set_buns(TestData.exempl_bun)
+        assert getattr(burger, 'bun') == TestData.exempl_bun
+
+
